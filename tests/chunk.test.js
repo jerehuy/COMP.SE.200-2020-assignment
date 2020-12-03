7baa6d8106d5c18ebe7693d552484d02c5fd897d
Pekka Lempiäinen <pekelem@gmail.com>
2020-12-03 12:28:39 +0200

    Added more chunk.js tests

--- tests/chunk.test.js
+++ tests/chunk.test.js
@@ -1,14 +1,43 @@
 import chunk from '../src/chunk'
 
-const abcd = ['a', 'b', 'c', 'd']
+const a = ['a', 'b', 'c', 'd']
 
 
 test('Chunk documentation example 1', () => {
-  expect( chunk(abcd, 2)).toBe([ ['a', 'b'], ['c', 'd'] ])
+  expect( chunk(a, 2)).toBe([ ['a', 'b'], ['c', 'd'] ])
 })
 
 
 test('Chunk documentation example 2', () => {
-  expect( chunk(abcd, 3)).toBe([ ['a', 'b', 'c'], ['d'] ])
+  expect( chunk(a, 3)).toMatchObject([ ['a', 'b', 'c'], ['d'] ])
 })
 
+
+test('divide into 4 arrays within an array', () => {
+  expect (chunk(a, 1)).toMatchObject( [ ['a'], ['b'], ['c'], ['d'] ])
+})
+
+
+describe('Sometimes this function adds undefined values to the to the arrays, these tests should reveal those defects', () => {
+            const a = ['a', 'b', 'c', 'd']
+            const cases = [ [a, 1], [a, 2], [a, 3], [a, 4] ];
+
+            test.each(cases)(
+              "%p, %p",
+              (firstArg, secondArg) => {
+                const result = chunk(firstArg, secondArg);
+                expect(result).not.toContain(undefined)
+              }
+            )
+        })
+
+
+
+
+test('empty array test with arg 1', () => {
+  expect( chunk([], 1)).toMatchObject([])
+})
+
+test('empty array test with arg 2', () => {
+  expect( chunk([])).toMatchObject([])
+})
