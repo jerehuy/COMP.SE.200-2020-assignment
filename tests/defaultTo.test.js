2ae2edcf1c92e42ae76c34fd180b4413223f5e2e
Pekka Lempiäinen <pekelem@gmail.com>
2020-12-07 16:50:09 +0200

    defaultTo tests added

--- tests/defaultTo.test.js
+++ tests/defaultTo.test.js
@@ -9,3 +9,37 @@ describe("documentation examples", () => {
         expect(defaultTo(undefined, 10)).toBe(10)
     })
 })
+
+describe("corner cases", () => {
+    test("0 shoudln't default", () => {
+        expect(defaultTo(0, 1)).toBe(0)
+    })
+
+    test("NaN should default", () => {
+        expect(defaultTo(NaN, 1)).toBe(1)
+    })
+
+    test("undefined should default", () => {
+        expect(defaultTo(undefined, 1)).toBe(1)
+    })
+
+    test("undefined should default", () => {
+        expect(defaultTo(null, 1)).toBe(1)
+    })
+
+    test("string '0' should defaultTo to default value", () => {
+        expect(defaultTo("0", 1)).toBe("0")
+    })
+
+    test("string '10' should default to default value", () => {
+        expect(defaultTo("10", 1)).toBe("10")
+    })
+
+    test("all parameters null", () => {
+        expect(defaultTo(null, null)).toBe(null)
+    })
+
+    test("parameters null, default to NaN", () => {
+        expect(defaultTo(null, NaN)).toBe(NaN)
+    })
+})
